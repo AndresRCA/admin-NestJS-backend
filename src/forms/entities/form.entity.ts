0eede899b95b3889d322a115bcd40f4ddb00edc9
AndresRCA <andresrca97@gmail.com>
2023-03-11 11:47:20 -0400

    remove deprecated dto

--- src/forms/entities/form.entity.ts
+++ src/forms/entities/form.entity.ts
@@ -1,8 +1,10 @@
+import { Type } from 'class-transformer';
 import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm'
 import { FormGroup } from './form-group.entity';
 
 @Entity({ schema: 'forms' })
 export class Form {
+  @Type(() => Number) // for transforming the string value that comes from a request
   @PrimaryGeneratedColumn({ type: "int" })
   id: number;
 
