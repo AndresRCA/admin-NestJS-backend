166a7d0fb5029a9bf17301633863902db28d8dc8
AndresRCA <andresrca97@gmail.com>
2023-04-14 17:01:15 -0400

    dbfull service expansion

--- src/config/dbfull-database.config.ts
+++ src/config/dbfull-database.config.ts
@@ -9,6 +9,7 @@ import { registerAs } from "@nestjs/config";
 export default registerAs('dbfull', () => ({
   url: process.env.DBFULL_API_URL,
   dbSae: process.env.DBFULL_DB_SAE,
+  dbClubFibex: process.env.DBFULL_DB_CLUBFIBEX,
   authToken: process.env.DBFULL_AUTH_TOKEN,
   authBasic: process.env.DBFULL_AUTH_BASIC,
   encryptKey: process.env.DBFULL_ENCRYPT_KEY
--- src/services/dbfull-client/dbfull-client.service.ts
+++ src/services/dbfull-client/dbfull-client.service.ts
@@ -20,8 +20,7 @@ export class DbfullClientService {
     this.dbFullClient = axios.create({
       headers: {
         'Authorization': 'Basic ' + Buffer.from(this.configService.get('dbfull.authBasic')!).toString('base64'),
-        'TokenAuthPlataform': this.configService.get('dbfull.authToken'),
-        'type': this.encryptionService.dbFullEncrypt('any-queries')
+        'TokenAuthPlataform': this.configService.get('dbfull.authToken')
       }
     });
   }
@@ -34,12 +33,71 @@ export class DbfullClientService {
    */
   public async execute(query: string, dbName: string) {
     this.configService.get('dbfull.url')
-    let res = await this.dbFullClient.get(this.configService.get('dbfull.url')!, {
+    let res = await this.dbFullClient.get(`${this.configService.get('dbfull.url')}/api`, {
       headers: {
         'db': this.encryptionService.dbFullEncrypt(dbName),
-        'x-data-query': this.encryptionService.dbFullEncrypt(query)
+        'x-data-query': this.encryptionService.dbFullEncrypt(query),
+        'type': this.encryptionService.dbFullEncrypt('any-queries')
       }
     });
     return res.data;
+  }
+
+  async GetAllData(table: string) {
+    return new Promise(async (resolve, reject) => {
+      axios.get(`/api/v1/find-all-info/${this.configService.get('dbfull.dbClubFibex')}/${table}`)
+        .then((resp: any) => resolve(resp.data))
+        .catch((error: any) => reject(error));
+    })
+  }
+
+  async GetAllT() {
+    return new Promise(async (resolve, reject) => {
+      axios.get(`/api/v1/show-all-info/${this.configService.get('dbfull.dbClubFibex')}`)
+        .then((resp: any) => resolve(resp.data))
+        .catch((error: any) => reject(error));
+    })
+  }
+
+  async GetAllDataCampo(table: string, campo: string, valor: any) {
+    const iURL = `/api/v1/find-any-info/${this.configService.get('dbfull.dbClubFibex')}/${table}/${campo}/${valor}`
+    console.log(iURL)
+    let res = await this.dbFullClient.get(iURL);
+    return res.data;
+  }
+
+  async GetAllEstrucura(table: string, campo: string, valor: any) {
+    //https://backend.thomas-talk.me/api/v1/find-any-info/thomas_clubfibex/cb_Estructura/TableName/cb_EmpresaConvenio
+    return new Promise(async (resolve, reject) => {
+      this.dbFullClient.get(`/api/v1/find-any-info/${this.configService.get('dbfull.dbClubFibex')}/${table}/${campo}/${valor}`)
+        .then((resp: any) => resolve(resp.data))
+        .catch((error: any) => reject(error));
+    })
+  }
+
+  async CreateData(table: string, idkey: string, body: any) {
+    return new Promise(async (resolve, reject) => {
+      console.log("body");
+      console.log(body);
+      console.log("key");
+      console.log(idkey)
+      console.log("table");
+      console.log(table);
+
+      let config = {
+        method: 'post',
+        url: `${this.configService.get('dbfull.url')}/api/v1/create-info/${this.configService.get('dbfull.dbClubFibex')}/${table}`,
+        headers: {
+          'x-keys-to-add-id': `["${idkey}"]`,
+          'x-keys-of-arrays': '[]',
+          'x-relations': 'false',
+          'Content-Type': 'application/json'
+        },
+        data: body
+      };
+      axios.request(config)
+        .then((response) => resolve(response.data))
+        .catch((error) => reject(error));
+    })
   }
 }
