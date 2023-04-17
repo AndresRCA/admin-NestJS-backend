54321cc2f2a287b6ff49ca397b4865c6d88b45c6
AndresRCA <andresrca97@gmail.com>
2023-04-17 16:10:04 -0400

    adjust code to equivalent form

--- src/services/dbfull-client/dbfull-client.service.ts
+++ src/services/dbfull-client/dbfull-client.service.ts
@@ -4,13 +4,12 @@ import axios, { AxiosInstance } from 'axios';
 import { EncryptionService } from '../encryption/encryption.service';
 
 /**
- * Servicio que se encarga de realizar operaciones con el backend dbfull, su 'type' por defecto para realizar
- * operaciones es 'any-queries'.
+ * Servicio que se encarga de realizar operaciones con el backend dbfull.
  */
 @Injectable()
 export class DbfullClientService {
   // instancia custom de axios que tendra la configuracion por defecto para cada operacion que involucre el backend dbfull
-  private readonly dbFullClient: AxiosInstance;
+  private readonly http: AxiosInstance;
 
   // no se para que se utiliza esto aqui en este servicio especificamente
   private ShowInfoTable: any = "";
@@ -20,7 +19,7 @@ export class DbfullClientService {
     private readonly encryptionService: EncryptionService
   ) {
     // inicializa un objeto custom de axios con la siguiente configuracion
-    this.dbFullClient = axios.create({
+    this.http = axios.create({
       headers: {
         'Authorization': 'Basic ' + Buffer.from(this.configService.get('dbfull.authBasic')!).toString('base64'),
         'TokenAuthPlataform': this.configService.get('dbfull.authToken')
@@ -28,7 +27,7 @@ export class DbfullClientService {
     });
 
     // No estoy seguro si este metodo actua como un tipo de cache para ShowInfoTable
-    this.ShowInfo();
+    this.showInfo();
   }
 
   /**
@@ -39,7 +38,7 @@ export class DbfullClientService {
    */
   public async execute(query: string, dbName: string): Promise<any> {
     this.configService.get('dbfull.url')
-    let res = await this.dbFullClient.get(`${this.configService.get('dbfull.url')}/api`, {
+    let res = await this.http.get(`${this.configService.get('dbfull.url')}/api`, {
       headers: {
         'db': this.encryptionService.dbFullEncrypt(dbName),
         'x-data-query': this.encryptionService.dbFullEncrypt(query),
@@ -49,128 +48,104 @@ export class DbfullClientService {
     return res.data;
   }
 
-  async GetAllData(table: string) {
-    return new Promise(async (resolve, reject) => {
-      axios.get(`/api/v1/find-all-info/${this.configService.get('dbfull.dbClubFibex')}/${table}`)
-        .then((resp: any) => resolve(resp.data))
-        .catch((error: any) => reject(error));
-    })
+  async getAllData(table: string) {
+    let resp = await this.http.get(`/api/v1/find-all-info/${this.configService.get('dbfull.dbClubFibex')}/${table}`);
+    return resp.data;
   }
 
-  async GetAllT() {
-    return new Promise(async (resolve, reject) => {
-      axios.get(`/api/v1/show-all-info/${this.configService.get('dbfull.dbClubFibex')}`)
-        .then((resp: any) => resolve(resp.data))
-        .catch((error: any) => reject(error));
-    })
+  async getAllT() {
+    let resp = await this.http.get(`/api/v1/show-all-info/${this.configService.get('dbfull.dbClubFibex')}`);
+    return resp.data;
   }
 
-  async GetAllDataCampo(table: string, campo: string, valor: any) {
-    const iURL = `/api/v1/find-any-info/${this.configService.get('dbfull.dbClubFibex')}/${table}/${campo}/${valor}`
-    console.log(iURL)
-    let res = await this.dbFullClient.get(iURL);
+  async getAllDataCampo(table: string, campo: string, valor: any) {
+    const iURL = `/api/v1/find-any-info/${this.configService.get('dbfull.dbClubFibex')}/${table}/${campo}/${valor}`;
+    console.log(iURL);
+    let res = await this.http.get(iURL);
     return res.data;
   }
 
-  async GetAllEstrucura(table: string, campo: string, valor: any) {
+  async getAllEstrucura(table: string, campo: string, valor: any) {
     //https://backend.thomas-talk.me/api/v1/find-any-info/thomas_clubfibex/cb_Estructura/TableName/cb_EmpresaConvenio
-    return new Promise(async (resolve, reject) => {
-      this.dbFullClient.get(`/api/v1/find-any-info/${this.configService.get('dbfull.dbClubFibex')}/${table}/${campo}/${valor}`)
-        .then((resp: any) => resolve(resp.data))
-        .catch((error: any) => reject(error));
-    })
+    let resp = await this.http.get(`/api/v1/find-any-info/${this.configService.get('dbfull.dbClubFibex')}/${table}/${campo}/${valor}`);
+    return resp.data;
   }
 
-  async CreateData(table: string, idkey: string, body: any) {
-    return new Promise(async (resolve, reject) => {
-      console.log("body");
-      console.log(body);
-      console.log("key");
-      console.log(idkey)
-      console.log("table");
-      console.log(table);
+  async createData(table: string, idkey: string, body: any) {
+    console.log("body");
+    console.log(body);
+    console.log("key");
+    console.log(idkey)
+    console.log("table");
+    console.log(table);
 
-      let config = {
-        method: 'post',
-        url: `${this.configService.get('dbfull.url')}/api/v1/create-info/${this.configService.get('dbfull.dbClubFibex')}/${table}`,
-        headers: {
-          'x-keys-to-add-id': `["${idkey}"]`,
-          'x-keys-of-arrays': '[]',
-          'x-relations': 'false',
-          'Content-Type': 'application/json'
-        },
-        data: body
-      };
-      axios.request(config)
-        .then((response: any) => resolve(response.data))
-        .catch((error: any) => reject(error));
-    })
+    let config = {
+      method: 'post',
+      url: `${this.configService.get('dbfull.url')}/api/v1/create-info/${this.configService.get('dbfull.dbClubFibex')}/${table}`,
+      headers: {
+        'x-keys-to-add-id': `["${idkey}"]`,
+        'x-keys-of-arrays': '[]',
+        'x-relations': 'false',
+        'Content-Type': 'application/json'
+      },
+      data: body
+    };
+    let res = await axios.request(config);
+    return res.data;
   }
 
-  async UpdateData(table: string, campoSearch: string, body: any) {
-    return new Promise(async (resolve, reject) => {
-      let config = {
-        method: 'put',
-        url: `${this.configService.get('dbfull.url')}/api/v1/update-info/${this.configService.get('dbfull.dbClubFibex')}/${table}/${campoSearch}`,
-        headers: {
-          'x-multiple-update': 'false',
-          'x-elements-obj': '[]',
-          'x-attr-duplicate': '[]',
-          'campo': '',
-          'Content-Type': 'application/json'
-        },
-        data: body
-      };
-      axios.request(config)
-        .then((response) => resolve(response.data))
-        .catch((error) => reject(error));
-    })
+  async updateData(table: string, campoSearch: string, body: any) {
+    let config = {
+      method: 'put',
+      url: `${this.configService.get('dbfull.url')}/api/v1/update-info/${this.configService.get('dbfull.dbClubFibex')}/${table}/${campoSearch}`,
+      headers: {
+        'x-multiple-update': 'false',
+        'x-elements-obj': '[]',
+        'x-attr-duplicate': '[]',
+        'campo': '',
+        'Content-Type': 'application/json'
+      },
+      data: body
+    };
+    let res = await axios.request(config);
+    return res.data;
   }
 
-  async ShowInfo() {
-    return new Promise(async (resolve, reject) => {
-      if (this.ShowInfoTable && this.ShowInfoTable != "") {
-        resolve(this.ShowInfoTable)
-      } else {
-        let config = {
-          method: 'get',
-          url: `${this.configService.get('dbfull.url')}/api/v1/show-all-info/${this.configService.get('dbfull.dbClubFibex')}`,
-        };
-        axios.request(config)
-          .then((response) => { resolve(response.data); this.ShowInfoTable = response.data })
-          .catch((error) => reject(error));
-      }
-    })
-  }
-
-  Filtrado(table: any) {
-    let info = this.ShowInfoTable.model.filter((datadb: any) => (datadb.tableName === table))
-    if (info && info.length > 0) {
-      info = info[0].attr.filter((camposexluido: any) => (camposexluido != "createdAt" && camposexluido != "updatedAt"));
-      return (info);
+  async showInfo() {
+    if (this.ShowInfoTable && this.ShowInfoTable != "") {
+      return this.ShowInfoTable;
     }
-    return [];
+
+    let config = {
+      method: 'get',
+      url: `${this.configService.get('dbfull.url')}/api/v1/show-all-info/${this.configService.get('dbfull.dbClubFibex')}`,
+    };
+    let res = await axios.request(config);
+    this.ShowInfoTable = res.data;
+    return res.data;
   }
 
-  async CamposTableReturn(table: string) {
-    return new Promise(async (resolve, reject) => {
-      if (table && table != "") {
-        if (this.ShowInfoTable && this.ShowInfoTable != "") {
-          let info = this.Filtrado(table);
-          return info;
-        } else {
-          this.ShowInfo()
-            .then((resp: any) => {
-              this.ShowInfoTable = resp;
-              let info = this.Filtrado(table);
-              return info;
-            })
-            .catch((error: any) => { reject({ error: error }) })
-        }
-      } else {
-        reject({ error: "Error debes poner un nombre de tabla" })
-      }
-    })
+  private filtrado(table: any) {
+    let info = this.ShowInfoTable.model.filter((datadb: any) => (datadb.tableName === table));
+    if (info.length > 0) {
+      info = info[0].attr.filter((camposexluido: any) => (camposexluido != "createdAt" && camposexluido != "updatedAt"));
+    }
+    return info; // retorna o un arreglo vacio (filter puede retornar vacio) o un arreglo modificado
   }
 
+  async camposTableReturn(table: string) {
+    if (!table || table == "") {
+      throw new Error("Error debes poner un nombre de tabla");
+    }
+
+    if (this.ShowInfoTable && this.ShowInfoTable != "") {
+      let info = this.filtrado(table);
+      return info;
+    }
+
+    let resp = await this.showInfo();
+    this.ShowInfoTable = resp;
+    let info = this.filtrado(table);
+    return info;
+  }
 }
